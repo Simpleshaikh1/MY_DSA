d76a09174f0c39297725d91b9de88fe82c8256b5
Toyyib <hamzatbolaji648@yahoo.com>
2024-12-01 22:36:07 +0100

    Pairsum using sorting method

--- Hashing/easyProblems/pairSum.js
+++ Hashing/easyProblems/pairSum.js
@@ -17,10 +17,39 @@ function pairSum(matrix, sum){
     return count
 }
 
+//Usng the sort method, 
+function pairSum(mat, n, sum){
+    for(let i = 0; i < n; i++){
+        mat[i].sort((a,b) => a - b);
+    }
+
+    for(let i = 0; i < n - 1; i++){
+        for(let j = i + 1; j < n; j++){
+            let left = 0, right = n - 1;
+
+            while(left < n && right >= 0){
+                if((mat[i][left] + mat[j][right]) ===  sum){
+                    console.log("(", mat[i][left], ", ", mat[j][right], "), ");
+
+                    left++;
+                    right--;
+                }else{
+                    if((mat[i][left] + mat[j][right]) < sum){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+            }
+        }
+    }
+}
+
 // Driver program to run the case 
 let sum = 11; 
+let n = 4;
 let mat = [[ 1, 3, 2, 4, ], 
            [5, 8, 7, 6, ], 
            [ 9, 10, 13, 11, ], 
             [12, 0, 14, 15,]]; 
-pairSum(mat, sum);
+pairSum(mat, n , sum);
